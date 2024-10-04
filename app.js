2080575e0379e0e4289e03aab909c93b421d83ea
weld1993 <christianspurlin2725@gmail.com>
2024-10-04 12:58:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const fetch = require('node-fetch');
-const accessToken = 'USER_ACCESS_TOKEN'; // You will replace this with the actual access token from OAuth flow
 
-// The Facebook Graph API URL to request user info
-const url = `https://graph.facebook.com/me?fields=age_range,birthday,gender,likes,location,email,public_profile&access_token=${accessToken}`;
+// Function to fetch Facebook user data using the access token
+const fetchFacebookUserData = async (accessToken) => {
+  const url = `https://graph.facebook.com/me?fields=age_range,birthday,gender,location,email&access_token=${accessToken}`;
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // This will contain the user's info
-  })
-  .catch(error => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.error) {
+      console.error('Error fetching user info:', data.error);
+      return null;
+    }
+
+    console.log('Fetched Facebook User Data:', data);
+    return data;
+  } catch (error) {
     console.error('Error fetching user info:', error);
-  });
+    return null;
+  }
+};
+
+module.exports = fetchFacebookUserData;
